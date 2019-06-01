784fea2670abcf030b398f4b514eda48dab2c31d
ri616 <ri616@ic.ac.uk>
2019-06-01 23:39:31 +0100

    Complete builder, refactor to use ops class

--- src/main/scala/algorithm/GeneticAlgorithm.scala
+++ src/main/scala/algorithm/GeneticAlgorithm.scala
@@ -1,32 +1,27 @@
 package algorithm
 
 import genotype.Genotype
+import util.Types.Population
 
 
-class GeneticAlgorithm[T] private(population: List[Genotype[T]],
-                                  numIter: Int,
-                                  fitnessOp: Genotype[T] => Float,
-                                  selectionOp: List[(Genotype[T], Float)] => List[Genotype[T]],
-                                  crossOverOp: (Genotype[T], Genotype[T]) => (Genotype[T], Genotype[T]) = null,
-                                  mutationOp: Genotype[T] => Genotype[T] = null,
-                                  mutationProb: Float = 1) {
+class GeneticAlgorithm[T](population: Population[T], numIter: Int, ops: GeneticAlgorithmOperators[T]) {
 
     def run(): List[Genotype[T]] = {
         var curPop = population
         for (_ <- 0 until numIter) {
-            curPop = selectionOp(evalPopulation(curPop))
+            curPop = ops.selectionOp(evalPopulation(curPop))
         }
         selectFittest(curPop)
     }
 
     private def evalPopulation(pop: List[Genotype[T]]): List[(Genotype[T], Float)] = {
-        population.map(genotype => (genotype, fitnessOp(genotype)))
+        population.map(genotype => (genotype, ops.fitnessOp(genotype)))
     }
 
     private def selectFittest(pop: List[Genotype[T]]): List[Genotype[T]] = {
         pop.foldLeft((List.empty[Genotype[T]], 0.0))((curr, genotype) => {
             val (fittest, bestFitness) = curr
-            val fitness = fitnessOp(genotype)
+            val fitness = ops.fitnessOp(genotype)
 
             if (fittest.isEmpty) {
                 (genotype :: fittest, fitness)
--- src/main/scala/algorithm/GeneticAlgorithmBuilder.scala
+++ src/main/scala/algorithm/GeneticAlgorithmBuilder.scala
@@ -6,6 +6,7 @@ import util.Types.{CrossoverOp, FitnessOp, MutationOp, Population, SelectionOp}
 class GeneticAlgorithmBuilder[T] {
 
     private var pop: Population[T] = _
+    private var numIterations: Int = _
     private var fitnessOp: FitnessOp[T] = _
     private var selectionOp: SelectionOp[T] = _
     private var crossoverOp: CrossoverOp[T] = _
@@ -13,11 +14,17 @@ class GeneticAlgorithmBuilder[T] {
     private var mutationProb: Float = 1
 
     def build(): GeneticAlgorithm[T] = {
-        new GeneticAlgorithm[T]()
+        val ops = new GeneticAlgorithmOperators[T](fitnessOp, selectionOp, crossoverOp, mutationOp, mutationProb)
+        new GeneticAlgorithm[T](pop, numIterations, ops)
     }
 
     def withPopulation(pop: Population[T]): GeneticAlgorithmBuilder[T] = {
         this.pop = pop
+        this
+    }
+
+    def withNumIterations(iters: Int): GeneticAlgorithmBuilder[T] = {
+        numIterations = iters
         this
     }
 
