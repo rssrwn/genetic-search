c20fb5ba73c636728eb00c49aeb8d848286069d9
ri616 <ri616@ic.ac.uk>
2019-06-01 23:39:03 +0100

    Add operators class for holding operators and data relating to ops

--- src/main/scala/algorithm/GeneticAlgorithmOperators.scala
+++ src/main/scala/algorithm/GeneticAlgorithmOperators.scala
@@ -1,5 +1,12 @@
 package algorithm
 
-class GeneticAlgorithmOperators {
+import util.Types.{CrossoverOp, FitnessOp, MutationOp, SelectionOp}
+
+
+class GeneticAlgorithmOperators[T](val fitnessOp: FitnessOp[T],
+                                   val selectionOp: SelectionOp[T],
+                                   val crossoverOp: CrossoverOp[T] = _,
+                                   val mutationOp: MutationOp[T] = _,
+                                   val mutationProb: Float = 1) {
 
 }
