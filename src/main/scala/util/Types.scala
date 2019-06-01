8a647a6b24c4075de979517b41c2007599f58820
ri616 <ri616@ic.ac.uk>
2019-06-01 23:30:20 +0100

    Refactor types

--- src/main/scala/util/Types.scala
+++ src/main/scala/util/Types.scala
@@ -1,5 +1,16 @@
 package util
 
+import genotype.Genotype
+
 object Types {
+
+    type Population[T] = List[Genotype[T]]
+    type EvalPopulation[T] = List[(Genotype[T], Float)]
+    private type GenotypePair[T] = (Genotype[T], Genotype[T])
+
+    type FitnessOp[T] = Genotype[T] => Float
+    type MutationOp[T] = Genotype[T] => Genotype[T]
+    type SelectionOp[T] = EvalPopulation[T] => Population[T]
+    type CrossoverOp[T] = GenotypePair[T] => GenotypePair[T]
 
 }
