5adc2d5e2899ef6d155d1e4b8dfb496c8302c961
ri616 <ri616@ic.ac.uk>
2019-06-04 21:03:54 +0100

    Add helpers for checking whether ops exist

--- src/main/scala/algorithm/GeneticAlgorithmOperators.scala
+++ src/main/scala/algorithm/GeneticAlgorithmOperators.scala
@@ -5,8 +5,16 @@ import util.Types.{CrossoverOp, FitnessOp, MutationOp, SelectionOp}
 
 class GeneticAlgorithmOperators[T](val fitnessOp: FitnessOp[T],
                                    val selectionOp: SelectionOp[T],
-                                   val crossoverOp: CrossoverOp[T] = _,
-                                   val mutationOp: MutationOp[T] = _,
+                                   val crossoverOp: CrossoverOp[T] = null,
+                                   val mutationOp: MutationOp[T] = null,
                                    val mutationProb: Float = 1) {
+
+    def containsCrossoverOp: Boolean = {
+        crossoverOp != null
+    }
+
+    def containsMutationOp: Boolean = {
+        mutationOp != null
+    }
 
 }
