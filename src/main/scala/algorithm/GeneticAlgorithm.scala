89b7ee150f3de0f53547568149a108f7a70fa842
ri616 <ri616@ic.ac.uk>
2019-06-04 21:08:14 +0100

    refactor args for GeneticAlgorithm

--- src/main/scala/algorithm/GeneticAlgorithm.scala
+++ src/main/scala/algorithm/GeneticAlgorithm.scala
@@ -4,10 +4,10 @@ import genotype.Genotype
 import util.Types.Population
 
 
-class GeneticAlgorithm[T](population: Population[T], numIter: Int, ops: GeneticAlgorithmOperators[T]) {
+class GeneticAlgorithm[T](ops: GeneticAlgorithmOperators[T], numIter: Int) {
 
-    def run(): Population[T] = {
-        var curPop = population
+    def run(pop: Population[T]): Population[T] = {
+        var curPop = pop
         for (_ <- 0 until numIter) {
             curPop = select(curPop)
             if (ops.containsCrossoverOp) {
@@ -21,12 +21,12 @@ class GeneticAlgorithm[T](population: Population[T], numIter: Int, ops: GeneticA
     }
 
     private def select(pop: Population[T]): Population[T] = {
-        val eval = population.map(genotype => (genotype, ops.fitnessOp(genotype)))
+        val eval = pop.map(genotype => (genotype, ops.fitnessOp(genotype)))
         ops.selectionOp(eval)
     }
 
     private def crossover(pop: Population[T]): Population[T] = {
-
+        
     }
 
     private def mutate(pop: Population[T]): Population[T] = {
