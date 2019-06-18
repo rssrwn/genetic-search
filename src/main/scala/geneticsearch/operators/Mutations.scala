beb21e36410700754acb1cb41fe7270a8ceff504
ri616 <ri616@ic.ac.uk>
2019-06-18 21:57:07 +0100

    Add initial implementation for mutation operator factory

---
+++ src/main/scala/geneticsearch/operators/Mutations.scala
@@ -0,0 +1,15 @@
+package geneticsearch.operators
+
+import geneticsearch.Types.MutationOp
+
+
+/*
+Factory for mutation operators
+ */
+object Mutations {
+
+    def randBitFlip[T]: MutationOp[T] = {
+
+    }
+
+}
