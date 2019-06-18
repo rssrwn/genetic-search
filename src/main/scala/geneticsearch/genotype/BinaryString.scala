f1a86c5f4877c17eecb5b7f83f3dc3d219285ff3
ri616 <ri616@ic.ac.uk>
2019-06-18 23:32:01 +0100

    Add mutation ability to BinaryString

--- src/main/scala/geneticsearch/genotype/BinaryString.scala
+++ src/main/scala/geneticsearch/genotype/BinaryString.scala
@@ -1,11 +1,14 @@
 package geneticsearch.genotype
 
+import scala.util.Random
+
 
 /**
   * Genotypes of binary strings
   * @param str Binary string to use as genotype, non-binary chars are ignored
+  * @param flipProb Probability of flipping a bit when mutating this genotype
   */
-class BinaryString(private val str: String) extends Genotype[BinaryString] with Seq[Int] {
+class BinaryString(private val str: String, private val flipProb: Float) extends Genotype[BinaryString] with Seq[Int] {
 
     private val binaryList: List[Int] = {
         str.toCharArray
@@ -29,12 +32,40 @@ class BinaryString(private val str: String) extends Genotype[BinaryString] with 
     override def split: (BinaryString, BinaryString) = {
         val midPoint = length / 2
         val (l1, l2) = binaryList.splitAt(midPoint)
-        (new BinaryString(l1.toString()), new BinaryString(l2.toString()))
+        (new BinaryString(l1.toString(), flipProb), new BinaryString(l2.toString(), flipProb))
     }
 
     override def merge(that: BinaryString): BinaryString = {
         val newStr = this.str + that.str
-        new BinaryString(newStr)
+        new BinaryString(newStr, flipProb)
+    }
+
+    override def mutate: Genotype[BinaryString] = {
+        val newStr = binaryList.map(bit => BinaryString.flipBit(bit, flipProb).toString)
+                .reduceLeft(_++_)
+
+        new BinaryString(newStr, flipProb)
+    }
+
+}
+
+object BinaryString {
+
+    /*
+    Flips bit with probability
+    Bit must be either 0 or 1
+     */
+    private def flipBit(bit: Int, prob: Float): Int = {
+        val rand = Random.nextFloat()
+        if (rand <= prob) {
+            if (bit == 0) {
+                1
+            } else {
+                0
+            }
+        } else {
+            bit
+        }
     }
 
 }
