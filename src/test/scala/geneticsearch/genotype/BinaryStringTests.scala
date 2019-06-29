08d809c86e865e8235ce5c5de6df9471cc2c7181
ri616 <ri616@ic.ac.uk>
2019-06-29 21:31:50 +0100

    Add tests for BinaryString mutation

--- src/test/scala/geneticsearch/genotype/BinaryStringTests.scala
+++ src/test/scala/geneticsearch/genotype/BinaryStringTests.scala
@@ -7,15 +7,17 @@ import org.scalatest.FunSuite
 
 class BinaryStringTests extends FunSuite {
 
+    private val flipProb = 0.2f
+
     test("A BinaryString has the expected length") {
-        val binStr = new BinaryString("00101")
+        val binStr = new BinaryString("00101", flipProb)
         val expectedLength = 5
 
         assertResult(expectedLength)(binStr.length)
     }
 
     test("A BinaryString ignores non-binary chars") {
-        val binStr = new BinaryString("00a10b1")
+        val binStr = new BinaryString("00a10b1", flipProb)
         val expectedLength = 5
         val intAtIndexTwo = 1
 
@@ -24,7 +26,7 @@ class BinaryStringTests extends FunSuite {
     }
 
     test("Splitting a BinaryString creates two BinaryStrings correctly") {
-        val binStr = new BinaryString("00101")
+        val binStr = new BinaryString("00101", flipProb)
         val (left: BinaryString, right) = extractSuccess(binStr.split(1))
 
         val expectedLeftLen = 2
@@ -41,12 +43,52 @@ class BinaryStringTests extends FunSuite {
     }
 
     test("Merging a split BinaryString creates an equivalent BinaryString") {
-        val binStr = new BinaryString("00101")
+        val binStr = new BinaryString("00101", flipProb)
         val (left, right) = extractSuccess(binStr.split(1))
 
         val newBinStr = left.merge(right)
 
-        assert(binStr.iterator.toList.equals(newBinStr.iterator.toList))
+        assert(binStr.equals(newBinStr))
+    }
+
+    test("Mutating a binaryString should do nothing when flipProb is zero") {
+        val binStr = new BinaryString("00101", 0)
+
+        val mutated = binStr.mutate()
+
+        assert(binStr.equals(mutated))
+    }
+
+    test("Mutating a binaryString should flip all bits when flipProb is one") {
+        val binStr = new BinaryString("00101", 1)
+
+        val mutated = binStr.mutate().asInstanceOf[BinaryString]
+
+        val expected = "11010"
+
+        assertResult(expected)(mutated.str)
+    }
+
+    test("Mutating a binaryString flips approx the correct number of bits") {
+        val str = "10" * 5000
+        val binStr = new BinaryString(str, flipProb)
+        val length = binStr.length
+
+        val mutated = binStr.mutate().asInstanceOf[BinaryString]
+
+        val errorRate = 0.02f
+        val acceptanceRange = ((flipProb - errorRate) * length, (flipProb + errorRate) * length)
+
+        val numMutations: Float = binStr.zipWithIndex.map { case(elem, idx) =>
+            if (elem == mutated(idx)) {
+                0.0f
+            } else {
+                1.0f
+            }
+        }.sum
+
+        assert(numMutations >= acceptanceRange._1)
+        assert(numMutations <= acceptanceRange._2)
     }
 
 }
