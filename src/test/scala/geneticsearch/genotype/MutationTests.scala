014cd92493c46a9d6fee2f98554a526b2f432c99
ri616 <ri616@ic.ac.uk>
2019-07-04 22:42:08 +0100

    Add tests for mutation bitflip genotype func

--- src/test/scala/geneticsearch/genotype/MutationTests.scala
+++ src/test/scala/geneticsearch/genotype/MutationTests.scala
@@ -1,48 +1,55 @@
 package geneticsearch.genotype
 
+import Mutation.bitFlip
 
-class MutationTests {
+import org.scalatest.FunSuite
 
-    // TODO
 
-//    test("Mutating a Sequence should do nothing when flipProb is zero") {
-//        val binStr = new BinaryString("00101", 0)
-//
-//        val mutated = binStr.mutate()
-//
-//        assert(binStr.equals(mutated))
-//    }
-//
-//    test("Mutating a binaryString should flip all bits when flipProb is one") {
-//        val binStr = new BinaryString("00101", 1)
-//
-//        val mutated = binStr.mutate().asInstanceOf[BinaryString]
-//
-//        val expected = "11010"
-//
-//        assertResult(expected)(mutated.str)
-//    }
-//
-//    test("Mutating a binaryString flips approx the correct number of bits") {
-//        val str = "10" * 5000
-//        val binStr = new BinaryString(str, flipProb)
-//        val length = binStr.length
-//
-//        val mutated = binStr.mutate().asInstanceOf[BinaryString]
-//
-//        val errorRate = 0.02f
-//        val acceptanceRange = ((flipProb - errorRate) * length, (flipProb + errorRate) * length)
-//
-//        val numMutations: Float = binStr.zipWithIndex.map { case(elem, idx) =>
-//            if (elem == mutated(idx)) {
-//                0.0f
-//            } else {
-//                1.0f
-//            }
-//        }.sum
-//
-//        assert(numMutations >= acceptanceRange._1)
-//        assert(numMutations <= acceptanceRange._2)
-//    }
+class MutationTests extends FunSuite {
+
+    private val binStr = new Sequence[Int](Seq(0,0,1,0,1), null, null)
+
+    test("bitFlip should do nothing when flipProb is zero") {
+        val flipFunc = bitFlip(0.0f)
+
+        val mutated = flipFunc(binStr)
+
+        assert(binStr.equals(mutated))
+    }
+
+    test("bitFlip should flip all bits when flipProb is one") {
+        val flipFunc = bitFlip(1.0f)
+
+        val mutated = flipFunc(binStr)
+
+        val expected = Seq(1,1,0,1,0)
+
+        assertResult(expected)(mutated)
+    }
+
+    test("bitFlip flips approx the correct number of bits") {
+        val str = "10" * 5000
+        val elems = Seq.tabulate(5000)(idx => str(idx).toInt)
+        val binStr = new Sequence[Int](elems, null, null)
+        val length = binStr.length
+
+        val flipProb = 0.2f
+        val flipFunc = bitFlip(flipProb)
+        val mutated = flipFunc(binStr)
+
+        val errorRate = 0.02f
+        val acceptanceRange = ((flipProb - errorRate) * length, (flipProb + errorRate) * length)
+
+        val numMutations: Int = binStr.zipWithIndex.map { case(elem, idx) =>
+            if (elem == mutated(idx)) {
+                0
+            } else {
+                1
+            }
+        }.sum
+
+        assert(numMutations >= acceptanceRange._1)
+        assert(numMutations <= acceptanceRange._2)
+    }
 
 }
