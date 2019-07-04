597e5ade093d282cd34155a06a7387a74d60ccff
ri616 <ri616@ic.ac.uk>
2019-07-04 22:22:48 +0100

    Add mutation func for doubles

--- src/main/scala/geneticsearch/genotype/Mutation.scala
+++ src/main/scala/geneticsearch/genotype/Mutation.scala
@@ -11,27 +11,51 @@ Factory for genotype mutation functions
 object Mutation {
 
     // TODO use BinaryString if created
+    // TODO use seq of bool for bin str?
+    /*
+    Returns a function for mutating binary strings by flipping bits with probability <flipProb>
+     */
     def bitFlip(flipProb: Float): MutationFunc[Sequence[Int]] = {
         genotype => {
-            val elems = genotype.map(bit => flipBit(bit, flipProb))
+            val elems = genotype.map { bit =>
+                val rand = Random.nextFloat()
+                if (rand <= flipProb) {
+                    if (bit == 0) {
+                        1
+                    } else {
+                        0
+                    }
+                } else {
+                    bit
+                }
+            }
             genotype.withElems(elems)
         }
     }
 
     /*
-    Flips bit with probability
-    Bit must be either 0 or 1
+    Returns a function for mutating a list of numbers by either adding or subtracting a small amount
+    The amount is the element multiplied by <mult>
+    Each element is mutated with probability <mutateProb>
      */
-    private def flipBit(bit: Int, prob: Float): Int = {
-        val rand = Random.nextFloat()
-        if (rand <= prob) {
-            if (bit == 0) {
-                1
-            } else {
-                0
+    def mutateDouble(mutateProb: Float, mult: Float): MutationFunc[Sequence[Double]] = {
+        genotype => {
+            val elems = genotype.map { elem =>
+                val rand = Random.nextFloat()
+                if (rand <= mutateProb) {
+                    val amt = elem * mult
+                    val inc = Random.nextFloat() >= 0.05
+                    if (inc) {
+                        elem + amt
+                    } else {
+                        elem - amt
+                    }
+                } else {
+                    elem
+                }
             }
-        } else {
-            bit
+
+            genotype.withElems(elems)
         }
     }
 
