4c8642c1d92ed62b3f279dca42281cfb0d4f4703
ri616 <ri616@ic.ac.uk>
2019-07-06 20:05:09 +0100

    Implemented mutation ops which take numToMutate arg

--- src/main/scala/geneticsearch/operators/Mutation.scala
+++ src/main/scala/geneticsearch/operators/Mutation.scala
@@ -12,7 +12,7 @@ object Mutation {
 
     /**
       * Returns a function which will choose to randomly mutate genotypes with proability <mutationProb>
-      * Mutated genotypes are then appended to the existing population
+      * Mutated genotypes are appended to the existing population
       * @param mutationProb Probability of mutating a genotype in the population
       * @tparam T Type of the elements within each genotype
       * @return Mutation operator
@@ -31,11 +31,26 @@ object Mutation {
         }
     }
 
-//    def appendMutatedPop[T](numToMutate: Int): MutationOp[T] = {
-//        pop: Population[T] => {
-//
-//        }
-//    }
+    /** Returns a function which will choose <numToMutate> genotypes randomly and mutate them
+      *  Mutated genotypes are appended to the existing population
+      * @param numToMutate Number of genotypes to be mutated (if greater than size of pop, all genotypes will be mutated)
+      * @tparam T Type of the elements within each genotype
+      * @return Mutation operator
+      */
+    def appendMutatedPop[T](numToMutate: Int): MutationOp[T] = {
+        pop: Population[T] => {
+            val idxs = for (_ <- 0 until numToMutate) yield Random.nextInt(numToMutate - 1)
+            val mutated = pop.zipWithIndex.flatMap { case(genotype, idx) =>
+                if (idxs.contains(idx)) {
+                    Some(genotype.mutate())
+                } else {
+                    None
+                }
+            }
+
+            pop ++ mutated
+        }
+    }
 
     /** Returns a function which will choose to randomly mutate genotypes with probability <mutationProb>
       * Mutated genotypes replace their initial genotypes in the new population
@@ -47,6 +62,25 @@ object Mutation {
         pop: Population[T] => {
             pop.map { genotype =>
                 if (Random.nextFloat() <= mutationProb) {
+                    genotype.mutate()
+                } else {
+                    genotype
+                }
+            }
+        }
+    }
+
+    /** Returns a function which will choose <numToMutate> genotypes randomly and mutate them
+      * Mutated genotypes replace their initial genotypes in the new population
+      * @param numToMutate Number of genotypes to be mutated (if greater than size of pop, all genotypes will be mutated)
+      * @tparam T Type of the elements within each genotype
+      * @return Mutation operator
+      */
+    def replaceWithMutated[T](numToMutate: Int): MutationOp[T] = {
+        pop: Population[T] => {
+            val idxs = for (_ <- 0 until numToMutate) yield Random.nextInt(numToMutate - 1)
+            pop.zipWithIndex.map { case(genotype, idx) =>
+                if (idxs.contains(idx)) {
                     genotype.mutate()
                 } else {
                     genotype
