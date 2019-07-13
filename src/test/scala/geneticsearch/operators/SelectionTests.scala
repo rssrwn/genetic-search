d16b93164e7f4bb5147e1bcc5e94be67b969b13f
ri616 <ri616@ic.ac.uk>
2019-07-13 16:51:23 +0100

    Add test for seletBest selction op

---
+++ src/test/scala/geneticsearch/operators/SelectionTests.scala
@@ -0,0 +1,29 @@
+package geneticsearch.operators
+
+import geneticsearch.genotype.Sequence
+import org.scalatest.FunSuite
+
+
+class SelectionTests extends FunSuite {
+
+    private val numToSelect = 2
+    private val selection = Selection.selectBest[Int](2)
+
+    private val pop = new Sequence[Int](Seq(1,2,3), null, null) ::
+            new Sequence[Int](Seq(11,12,130), null, null) ::
+            new Sequence[Int](Seq(21,22,23), null, null) :: Nil
+
+    test("selectBest returns a pop of correct size") {
+        val eval = Seq(3.0,2.0,6.0)
+        val evalPop = pop.zip(eval)
+
+        val selected = selection(evalPop)
+
+        val expectedBest = Seq(21,22,23)
+        val expectedSecond = Seq(1,2,3)
+
+        assertResult(expectedBest)(selected.head)
+        assertResult(expectedSecond)(selected(1))
+    }
+
+}
