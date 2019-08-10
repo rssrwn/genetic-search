117abd8245f21dbecdb72e12f653ee3967c7f6a0
ri616 <rssrwn@gmail.com>
2019-08-10 21:24:27 +0100

    Add tournament selection op and tests

--- src/main/scala/geneticsearch/operators/Selection.scala
+++ src/main/scala/geneticsearch/operators/Selection.scala
@@ -1,20 +1,63 @@
 package geneticsearch.operators
 
 import geneticsearch.Types.SelectionOp
+import geneticsearch.genotype.Genotype
+
+import scala.util.Random
 
 
 object Selection {
 
     /**
       * Returns a function which takes the top <numToSelect> genotypes from the population
+      *
       * @param numToSelect The number of genotypes to be selected
       * @tparam T Type of the elements within the genotype
-      * @return Selection function
+      * @return Selection operator
       */
     def selectBest[T](numToSelect: Int): SelectionOp[T] = {
         evalPop => {
             evalPop.sortWith(_._2 > _._2)
                     .take(numToSelect)
+        }
+    }
+
+    /**
+      * Returns a function which selects genotypes based on a tournament.
+      * For each genotype to be selected two genotypes are chosen at random from current pop
+      * and the genotype with higher fitness is added to the next pop
+      *
+      * @param numToSelect Number of genotypes to be selected
+      * @param elitism Number of times the fittest genotype is guaranteed to be in the next pop (max <numToSelect>)
+      * @tparam T Type of the elements within the genotype
+      * @return Selection operator
+      */
+    def tournament[T](numToSelect: Int, elitism: Int = 0): SelectionOp[T] = {
+        evalPop => {
+            val popSize = evalPop.length
+            val elitismToSelect = if (elitism >= numToSelect) {
+                numToSelect
+            } else {
+                elitism
+            }
+
+            val initPop = if (elitismToSelect > 0) {
+                val bestElem = evalPop.sortWith(_._2 > _._2).head
+                (for (_ <- 0 until elitismToSelect) yield bestElem).toVector
+            } else {
+                Vector.empty[(Genotype[T], Double)]
+            }
+
+            (0 until (numToSelect - elitismToSelect)).foldLeft(initPop) { case (pop, _) =>
+                val elem1 = evalPop(Random.nextInt(popSize))
+                val elem2 = evalPop(Random.nextInt(popSize))
+
+                if (elem1._2 > elem2._2) {
+                    pop :+ elem1
+                } else {
+                    pop :+ elem2
+                }
+            }
         }
     }
 
--- src/test/scala/geneticsearch/operators/SelectionTests.scala
+++ src/test/scala/geneticsearch/operators/SelectionTests.scala
@@ -7,7 +7,10 @@ import org.scalatest.FunSuite
 class SelectionTests extends FunSuite {
 
     private val numToSelect = 2
-    private val selection = Selection.selectBest[Int](numToSelect)
+    private val elitism = 2
+    private val selectBest = Selection.selectBest[Int](numToSelect)
+    private val tournament = Selection.tournament[Int](numToSelect)
+    private val tournamentWithElitism = Selection.tournament[Int](numToSelect, elitism)
 
     private val pop = (new Sequence[Int](Seq(1,2,3), null, null) ::
             new Sequence[Int](Seq(11,12,13), null, null) ::
@@ -17,13 +20,58 @@ class SelectionTests extends FunSuite {
         val eval = Seq(3.0,2.0,6.0)
         val evalPop = pop.zip(eval)
 
-        val selected = selection(evalPop)
+        val selected = selectBest(evalPop)
+
+        assertResult(numToSelect)(selected.length)
+    }
+
+    test("selectBest returns the expected genotypes") {
+        val eval = Seq(3.0,2.0,6.0)
+        val evalPop = pop.zip(eval)
+
+        val selected = selectBest(evalPop)
 
         val expectedBest = (Seq(21,22,23),6.0)
         val expectedSecond = (Seq(1,2,3),3.0)
 
         assertResult(expectedBest)(selected.head)
         assertResult(expectedSecond)(selected(1))
+    }
+
+    test("tournament returns a pop of correct size") {
+        val eval = Seq(3.0,2.0,6.0)
+        val evalPop = pop.zip(eval)
+
+        val selected = tournament(evalPop)
+
+        val elitismSelected = tournamentWithElitism(evalPop)
+
+        assertResult(numToSelect)(selected.length)
+        assertResult(numToSelect)(elitismSelected.length)
+    }
+
+    test("tournament returns the expected genotypes") {
+        val eval = Seq(3.0,2.0,6.0)
+        val evalPop = pop.zip(eval)
+
+        val selected = tournamentWithElitism(evalPop)
+
+        val expected = (Seq(21,22,23),6.0)
+
+        assertResult(expected)(selected.head)
+        assertResult(expected)(selected(1))
+    }
+
+    test("tournament returns a pop of expected size when elitism is greater than numToSelect") {
+        val eval = Seq(3.0,2.0,6.0)
+        val evalPop = pop.zip(eval)
+
+        val elitism = 3
+        val tournamentWithElitism = Selection.tournament[Int](numToSelect, elitism)
+
+        val selected = tournamentWithElitism(evalPop)
+
+        assertResult(numToSelect)(selected.length)
     }
 
 }
